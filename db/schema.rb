2169151707a4c76e03881bda3c44e055e042dbb8
Larry Norton <larryn@flashmind.net>
2015-01-17 20:08:52 +0000

    added Comment scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116232003) do
+ActiveRecord::Schema.define(version: 20150117175624) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "event_id"
+    t.text     "details"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.string   "title"
